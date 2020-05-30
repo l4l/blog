a42013b276d5a3f93a53a306b52d5af0187b77a1
Kitsu <mail@kitsu.me>
2020-05-30 21:36:00 +0300

    Publish post from drafts

--- new-post.py
+++ new-post.py
@@ -2,37 +2,73 @@
 
 from datetime import datetime
 from sys import argv
-from os.path import isfile
-from os import rename
+from os import rename, path, listdir
+
+DRAFT_PATH = 'draft/'
+POST_PATH = 'public/posts/'
 
 
-if len(argv) > 2:
-    print('too much arguments')
+def post_filename(name):
+    assert name.find('_') == -1
+    return name.lower().replace(' ', '_') + '.md'
+
+
+def create_draft(name):
+    filepath = DRAFT_PATH + post_filename(name)
+
+    if path.exists(filepath):
+        print('Filename already exists, still proceed? y/N')
+        yn = input().lower()
+
+        if yn != 'y' and yn != 'yes':
+            exit(3)
+        else:
+            rename(filepath, filepath + '.bak')
+
+    with open(filepath, 'w+') as f:
+        f.write('# {}\n\nLorem ipsum'.format(name))
+
+
+def publish(name):
+    draft_path = DRAFT_PATH + post_filename(name)
+
+    if not path.exists(draft_path):
+        print('Draft `{}` does not exist'.format(name))
+        exit(4)
+
+    for item in listdir(POST_PATH):
+        if item.find(post_filename(name)) != -1:
+            print('Post `{}` already published as {}'.format(name, item))
+            exit(4)
+
+    now = datetime.now()
+    filename = POST_PATH + now.strftime('%Y_%m_%d_') + post_filename(name)
+    rename(draft_path, filename)
+
+    with open(filename, 'r+') as f:
+        content = f.read()
+        f.seek(0, 0)
+        f.write('<!-- {} -->\n{}'.format(now.strftime('%Y/%m/%d %H:%M:%S'), content))
+
+
+USAGE = 'Usage: {} (draft|publish) <post-name>'.format(argv[0])
+
+if len(argv) != 3:
+    print(USAGE)
     exit(1)
-elif len(argv) == 2:
-    name = argv[1]
-else:
-    print('Enter post name:')
-    name = input()
+
+cmd = argv[1]
+name = argv[2]
 
 if name.find('_') != -1:
     print("Name cannot contain '_' symbol")
     exit(2)
 
-now = datetime.now()
-filename = '{:04}_{:02}_{:02}_{}'.format(now.year, now.month, now.day, name.lower().replace(' ', '_'))
-print('Going to create file', filename)
-
-filepath = 'public/posts/' + filename + '.md'
-if isfile(filepath):
-    print('Filename already exists, still proceed? y/N')
-    yn = input().lower()
-
-    if yn != 'y' and yn != 'yes':
-        exit(3)
-    else:
-        rename(filepath, filepath + '.bak')
-
-
-with open(filepath, 'w+') as f:
-    f.write('# {}\n\nLorem ipsum'.format(name))
+if cmd == 'draft':
+    create_draft(name)
+elif cmd == 'publish':
+    publish(name)
+else:
+    print('Unknown command')
+    print(USAGE)
+    exit(1)
